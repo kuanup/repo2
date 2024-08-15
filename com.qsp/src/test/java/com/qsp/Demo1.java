575d128fe3a52d72d14f5e06c07210a9a268f965
kuanu <kuanu@Anup_Kumar>
2024-08-15 11:52:31 +0530

    frist push

---
+++ com.qsp/src/test/java/com/qsp/Demo1.java
@@ -0,0 +1,23 @@
+package com.qsp;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+@Test
+	public void lounchBrowser() {
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	
+	driver.get("https://demowebshop.tricentis.com/");
+	Reporter.log("welcome page is display",true);
+	
+	}
+	}
+
+
