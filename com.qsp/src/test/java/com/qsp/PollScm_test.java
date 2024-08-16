04a0b5bfbe48e35c7a8cc5e58dca1b5a31fa24f4
kuanu <kuanu@Anup_Kumar>
2024-08-16 11:54:55 +0530

    poll scm added

---
+++ com.qsp/src/test/java/com/qsp/PollScm_test.java
@@ -0,0 +1,13 @@
+package com.qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScm_test {
+	@Test
+	public void poll() {
+		Reporter.log("Poll scm class execute ",true);
+	}
+	
+
+}
